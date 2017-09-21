91f0e9c1f0ce4f16af21c9bf1ddcfcce41f6d931
OmManiPemeHung <danielwraga@gmail.com>
2017-09-21 22:25:29 +0200

    poprawione

--- zadanie10-2.js
+++ zadanie10-2.js
@@ -1,15 +1,22 @@
 $(function(){
 	//this code will execute after the DOM is loaded
+	
 	var carouselList = $("#carousel ul");
 	
-	setInterval((changeSlide, 3000){
-	carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-	});
+	setInterval(changeSlide, 3000);
 
-	moveFirstSlide(){
+	function changeSlide() {
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+	}
+
+	function moveFirstSlide(){
 	var firstItem = carouselList.find("li:first");
 	var lastItem = carouselList.find("li:last");
-	};
+
+	lastItem.after(firstItem);
+	carouselList.css({marginLeft: 0});
+
+	}
 });
 
 
